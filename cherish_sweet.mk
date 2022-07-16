bb6320745be11311cc7bf04c365a12495f934e3a
Niranjan BR <niranjankannan2003@gmail.com>
2022-07-16 06:39:51 +0000

    Welcome Cherish OS
    Signed-off-by: Niranjan BR <niranjankannan2003@gmail.com>

--- cherish_sweet.mk
+++ cherish_sweet.mk
@@ -11,14 +11,14 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from sweet device
 $(call inherit-product, device/xiaomi/sweet/device.mk)
 
-# Inherit some common PixelOS stuff.
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+# Inherit some common Cherish OS stuff.
+$(call inherit-product, vendor/cherish/config/common_full_phone.mk)
 TARGET_GAPPS_ARCH := arm64
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_SUPPORTS_QUICK_TAP := true
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := aosp_sweet
+PRODUCT_NAME := cherish_sweet
 
 PRODUCT_DEVICE := sweet
 PRODUCT_BRAND := Xiaomi
@@ -26,3 +26,8 @@ PRODUCT_MODEL := Redmi Note 10 Pro
 PRODUCT_MANUFACTURER := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+# Cherish OS Stuffs
+CHERISH_BUILD_TYPE=OFFICIAL
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    ro.cherish.maintainer=Niranjan&Madhav
